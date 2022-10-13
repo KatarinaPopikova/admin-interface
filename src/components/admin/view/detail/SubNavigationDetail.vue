78e91a766c602cbb1a097dbba446f8b581ec1e2c
kStasova <stasovakatka6688@gmail.com>
2022-10-13 23:10:19 +0200

    feat: add back icon from Detail

--- src/components/admin/view/detail/SubNavigationDetail.vue
+++ src/components/admin/view/detail/SubNavigationDetail.vue
@@ -1,14 +1,26 @@
 <template>
-  <nav>
-    <router-link :to="`/${$i18n.locale}/admin/${activityId}/activity`"
-      >Activity</router-link
-    >|
-    <router-link :to="`/${$i18n.locale}/admin/${activityId}/ticket`"
-      >Tickets</router-link
-    >|
-    <router-link :to="`/${$i18n.locale}/admin/${activityId}/summary`"
-      >Summaries</router-link
-    >
+  <nav class="flex items-center justify-between">
+    <div>
+      <router-link :to="`/${$i18n.locale}/admin/${activityId}/activity`"
+        >Activity</router-link
+      >|
+      <router-link :to="`/${$i18n.locale}/admin/${activityId}/ticket`"
+        >Tickets</router-link
+      >|
+      <router-link :to="`/${$i18n.locale}/admin/${activityId}/summary`"
+        >Summaries</router-link
+      >
+    </div>
+
+    <div>
+      <router-link :to="`/${$i18n.locale}/admin/overview/activity`">
+        <font-awesome-icon
+          icon="fa-solid fa-arrow-left"
+          title="Back"
+          class="text-center text-main-color-400 hover:text-main-color-500 hover:cursor-pointer h-10 w-10"
+        />
+      </router-link>
+    </div>
   </nav>
 </template>
 
