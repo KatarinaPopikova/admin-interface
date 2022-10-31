276e2d798717412977c40450f3c8075e5680684f
kStasova <stasovakatka6688@gmail.com>
2022-10-31 18:25:06 +0100

    fix: manage with color on hover

--- src/components/admin/view/overview/summary/ActivityPicker.vue
+++ src/components/admin/view/overview/summary/ActivityPicker.vue
@@ -1,93 +1,77 @@
 <template>
   <div class="flex flex-col items-center">
-    <div class="flex flex-col items-center h-64">
-      <div class="w-full px-4">
-        <div class="flex flex-col items-center relative">
-          <div class="w-full">
-            <div
-              v-click-away="close"
-              class="my-2 p-1 bg-white flex border border-gray-200 rounded"
-            >
-              <input
-                v-model="filter"
-                @mousedown="open()"
-                @keydown.enter.stop.prevent="selectOption()"
-                class="p-1 px-2 appearance-none outline-none w-full text-gray-800"
-              />
-              <div
-                class="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200"
+    <div class="w-full px-4">
+      <div class="flex flex-col items-center relative">
+        <div class="w-full">
+          <div
+            v-click-away="close"
+            class="p-1 bg-white flex border border-gray-200 rounded"
+          >
+            <input
+              v-model="filter"
+              @mousedown="open()"
+              placeholder="Select activity"
+              @keydown.enter.stop.prevent="selectOption()"
+              class="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+            />
+            <div class="text-gray-300 w-8 py-1 pl-2 pr-1 flex items-center">
+              <button
+                @click="toggle()"
+                class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none"
               >
-                <button
-                  @click="toggle()"
-                  class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="100%"
+                  height="100%"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="100%"
-                    height="100%"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <polyline
-                      v-if="!isOpen()"
-                      points="18 15 12 20 6 15"
-                    ></polyline>
-                    <polyline
-                      v-if="isOpen()"
-                      points="18 15 12 9 6 15"
-                    ></polyline>
-                  </svg>
-                </button>
-              </div>
+                  <polyline
+                    v-if="!isOpen()"
+                    points="18 15 12 20 6 15"
+                  ></polyline>
+                  <polyline v-if="isOpen()" points="18 15 12 9 6 15"></polyline>
+                </svg>
+              </button>
             </div>
           </div>
-          <div
-            v-if="isOpen()"
-            class="absolute shadow bg-white top-100 z-40 w-full lef-0 rounded max-h-select overflow-y-auto svelte-5uyqqj"
-          >
-            <div class="flex flex-col w-full">
-              <template
-                v-for="(option, index) in filteredOptions()"
-                :key="index"
+        </div>
+        <div
+          v-if="isOpen()"
+          class="absolute shadow bg-white top-100 z-40 md:w-full w-fit min-w-fit left-0 rounded max-h-select overflow-y-auto"
+        >
+          <div class="flex flex-col w-full">
+            <template v-for="(option, index) in filteredOptions()" :key="index">
+              <div
+                @click="onOptionClick(index)"
+                :class="classOption(option.login.uuid, index)"
+                :aria-selected="focusedOptionIndex === index"
               >
                 <div
-                  @click="onOptionClick(index)"
-                  :class="classOption(option.login.uuid, index)"
-                  :aria-selected="focusedOptionIndex === index"
+                  class="flex w-fit items-center p-2 pl-2 border-transparent border-l-2 relative hover:border-teal-100"
                 >
-                  <div
-                    class="flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative hover:border-teal-100"
-                  >
-                    <div class="w-6 flex flex-col items-center">
-                      <div
-                        class="flex relative w-5 h-5 bg-orange-500 justify-center items-center m-1 mr-2 w-4 h-4 mt-1 rounded-full"
-                      >
-                        <img
-                          class="rounded-full"
-                          alt="A"
-                          v-bind:src="option.picture.thumbnail"
-                        />
-                      </div>
+                  <div class="w-8 flex flex-col items-center">
+                    <div
+                      class="flex relative w-8 h-8 justify-center items-center m-1 mr-2 w-4 h-4 mt-1 rounded-full"
+                    >
+                      <img
+                        alt="option.email"
+                        src="https://via.placeholder.com/150x100"
+                      />
                     </div>
-                    <div class="w-full items-center flex">
-                      <div class="mx-2 -mt-1">
-                        <span
-                          v-text="option.name.first + ' ' + option.name.last"
-                        ></span>
-                        <div
-                          class="text-xs truncate w-full normal-case font-normal -mt-1 text-gray-500"
-                          v-text="option.email"
-                        ></div>
-                      </div>
+                  </div>
+                  <div class="w-fit items-center flex">
+                    <div class="mx-2 -mt-1">
+                      <span v-text="option.email"></span>
                     </div>
                   </div>
                 </div>
-              </template>
-            </div>
+              </div>
+            </template>
           </div>
         </div>
       </div>
@@ -144,15 +128,13 @@ export default defineComponent({
       const isSelected = this.selected
         ? id === this.selected.login.uuid
         : false;
-      const isFocused = index === this.focusedOptionIndex;
       return {
-        "cursor-pointer w-full border-gray-100 border-b hover:bg-blue-50": true,
-        "bg-blue-100": isSelected,
-        "bg-blue-50": isFocused,
+        "cursor-pointer w-full border-gray-100 border-b hover:bg-gray-200 hover:opacity-70": true,
+        "bg-gray-200": isSelected,
       };
     },
     fetchOptions() {
-      fetch("https://randomuser.me/api/?results=5")
+      fetch("https://randomuser.me/api/?results=8")
         .then((response) => response.json())
         .then((data) => (this.options = data));
     },
