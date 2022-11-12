7c43c6c74b1f726807e2423fe4b6400abcdfa245
kStasova <stasovakatka6688@gmail.com>
2022-11-12 22:51:33 +0100

    fix: add back button for email editor

--- src/views/admin/detail/email-editor/EmailEditorView.vue
+++ src/views/admin/detail/email-editor/EmailEditorView.vue
@@ -1,5 +1,18 @@
 <template>
-  <EmailReader />
+  <div class="relative">
+    <div class="-top-8 right-2 absolute">
+      <router-link
+        :to="`/${$i18n.locale}/admin/${this.$route.params.id}/activity`"
+      >
+        <font-awesome-icon
+          icon="fa-solid fa-arrow-left"
+          title="Back"
+          class="text-main-color-400 hover:text-main-color-500 hover:cursor-pointer h-10 w-10 mr-7"
+        />
+      </router-link>
+    </div>
+    <EmailReader />
+  </div>
 </template>
 
 <script lang="ts">
