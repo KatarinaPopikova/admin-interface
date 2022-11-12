a1598d451af954e6df2fa546b43ce075f21396c2
kStasova <stasovakatka6688@gmail.com>
2022-11-12 23:09:38 +0100

    fix: allow access to the editor link only after click on icon (not col)

--- src/components/admin/view/detail/activity/ActivityEmailTableRow.vue
+++ src/components/admin/view/detail/activity/ActivityEmailTableRow.vue
@@ -1,25 +1,25 @@
 <template>
   <tr class="border-b border-gray-200 hover:bg-gray-100">
-    <router-link
-      :to="`/${$i18n.locale}/admin/${this.$route.params.id}/activity/email/${email_id}`"
-    >
-      <td class="py-3 text-left whitespace-nowrap">
-        <input
-          :value="`${email}`"
-          @change="editEmail($event.target.value)"
-          onkeydown="this.style.width = (this.value.length + 2 ) + 'ch';"
-          v-bind:style="{ width: email.length + 2 + 'ch' }"
-          v-bind:class="{
-            'outline-none text-gray-500': !editable,
-            'border-2 border-black': editable,
-          }"
-          :readonly="!editable"
-          :name="email"
-          class="bg-transparent block ml-4 pl-1.5 p-0.5 rounded-lg"
-        />
-      </td>
-      <td class="py-3 px-6 text-center">
-        <div class="w-4">
+    <td class="py-3 text-left whitespace-nowrap">
+      <input
+        :value="`${email}`"
+        @change="editEmail($event.target.value)"
+        onkeydown="this.style.width = (this.value.length + 2 ) + 'ch';"
+        v-bind:style="{ width: email.length + 2 + 'ch' }"
+        v-bind:class="{
+          'outline-none text-gray-500': !editable,
+          'border-2 border-black': editable,
+        }"
+        :readonly="!editable"
+        :name="email"
+        class="bg-transparent block ml-4 pl-1.5 p-0.5 rounded-lg"
+      />
+    </td>
+    <td class="py-3 px-6 text-center">
+      <div class="w-4">
+        <router-link
+          :to="`/${$i18n.locale}/admin/${this.$route.params.id}/activity/email/${email_id}`"
+        >
           <font-awesome-icon
             icon="fa-regular fa-pen-to-square"
             title="Edit"
@@ -27,26 +27,26 @@
             class="input-manager-icon"
             @click="startEditInputArea"
           />
-        </div>
-        <div class="w-4 flex">
-          <font-awesome-icon
-            icon="fa-solid fa-check"
-            title="Save"
-            v-if="editable"
-            class="input-manager-icon"
-            @click="saveEditedInputArea"
-          />
+        </router-link>
+      </div>
+      <div class="w-4 flex">
+        <font-awesome-icon
+          icon="fa-solid fa-check"
+          title="Save"
+          v-if="editable"
+          class="input-manager-icon"
+          @click="saveEditedInputArea"
+        />
 
-          <font-awesome-icon
-            icon="fa-solid fa-xmark"
-            title="Do not save"
-            v-if="editable"
-            class="input-manager-icon"
-            @click="restoreEditedInputArea"
-          />
-        </div>
-      </td>
-    </router-link>
+        <font-awesome-icon
+          icon="fa-solid fa-xmark"
+          title="Do not save"
+          v-if="editable"
+          class="input-manager-icon"
+          @click="restoreEditedInputArea"
+        />
+      </div>
+    </td>
   </tr>
 </template>
 
